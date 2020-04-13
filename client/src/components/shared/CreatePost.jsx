57f358b495e7c90b1cc49f8a51432340aeb01690
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-04-13 13:55:33 -0400

    post still works and just added image upload to the cloud

--- client/src/components/shared/CreatePost.jsx
+++ client/src/components/shared/CreatePost.jsx
@@ -1,28 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 
 function CreatePost(props) {
+  //help with image upload for React/cloudinary https://www.youtube.com/watch?v=hlYczGvLlDY
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "instashop");
+    setLoading(true);
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dolrvzlkq/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    const file = await res.json();
+
+    setImage(file.secure_url);
+    setLoading(false);
+  };
+
   return (
     <div className="create-form">
-      <h3 className="createposth3">Create Post</h3>
-      <form onSubmit={props.newPost}>
-        <textarea
-          type="text"
-          placeholder="Enter Post Caption"
-          name="caption"
-          value={props.postForm.caption}
-          onChange={props.handleFormChange}
-        />
+      <div>
+        <h3 className="createposth3">Create Post</h3>
 
-        <input
-          type="text"
-          placeholder="Enter Link to Product"
-          name="link_to_product"
-          value={props.postForm.link_to_product}
-          onChange={props.handleFormChange}
-        />
-        <button>Create Post</button>
-      </form>
+        <label htmlFor="file">
+          Upload Image
+          <input
+            type="file"
+            name="file"
+            onChange={uploadImage}
+          />
+        </label>
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          <img
+            src={image}
+            style={{ maxWidth: "300px", maxHeight: "300px" }}
+            alt="post"
+          />
+        )}
+      </div>
+      <div>
+        <form onSubmit={props.newPost}>
+          <textarea
+            type="text"
+            placeholder="Enter Post Caption"
+            name="caption"
+            value={props.postForm.caption}
+            onChange={props.handleFormChange}
+          />
+
+          <input
+            type="text"
+            placeholder="Enter Link to Product"
+            name="link_to_product"
+            value={props.postForm.link_to_product}
+            onChange={props.handleFormChange}
+          />
+          <button>Create Post</button>
+        </form>
+      </div>
     </div>
   );
 }
