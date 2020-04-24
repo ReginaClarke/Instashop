d214a077f79580bb63580744f0402e6ea719a882
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-04-24 15:19:48 -0400

    reduced the size of boxes and header for mobile

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 
 const Header = (props) => (
   <header>
-    <h1>Instashop</h1>
+    <h1 className="header">Instashop</h1>
     <Navigation
       currentUser={props.currentUser}
     handleLogout={props.handleLogout}
