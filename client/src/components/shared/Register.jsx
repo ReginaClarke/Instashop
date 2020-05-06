b145a7bfaeeebd5ef0f2f8afc3bf33725c417bba
ReginaClarke <58581262+ReginaClarke@users.noreply.github.com>
2020-05-06 18:33:21 -0400

    adjusted landing page image size

--- client/src/components/shared/Register.jsx
+++ client/src/components/shared/Register.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { withRouter } from 'react-router-dom';
 
 const Register = (props) => {
   return (
@@ -98,8 +99,15 @@ const Register = (props) => {
         <hr />
         <button>Register</button>
       </form>
+      <button
+           onClick={() => {
+            props.history.push("/")
+          }}
+      >
+        Cancel
+      </button>
     </div>
   );
 };
 
-export default Register;
+export default withRouter(Register);
